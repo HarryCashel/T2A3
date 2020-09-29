e6090f2651f661342a56056e3bc118db1e96c397
HarryCashel <65519528+HarryCashel@users.noreply.github.com>
2020-09-29 12:35:56 +1000

    Finished authorisation.py Added function to refresh access token

--- src/authorisation.py
+++ src/authorisation.py
@@ -9,7 +9,6 @@ import pkce
 import urllib.request
 import webbrowser
 from credentials import *
-# print(client_id)
 
 
 class PkceAuthCode(object):
@@ -63,7 +62,15 @@ class PkceAuthCode(object):
                           "response_type": "code",
                           "redirect_uri": redirect_uri,
                           "code_challenge_method": "S256",
-                          "code_challenge": get_challenge_code()
+                          "code_challenge": code_challenge,
+                          "scope": """
+                          user-read-private 
+                          playlist-modify-public 
+                          user-library-modify 
+                          user-top-read 
+                          playlist-modify-private 
+                          user-library-read
+                          """
                           })
         lookup_url = f"{authorisation_uri}?{data}"
         return lookup_url
@@ -72,15 +79,56 @@ class PkceAuthCode(object):
         lookup_url = self.get_auth_uri()
         webbrowser.open_new(lookup_url)
 
-    def get_auth(self):
+    # def get_auth(self):
+    #     access_token_url = self.access_token_url
+    #     access_token_data = self.get_access_token_data()
+    #     access_token_headers = self.get_access_token_headers()
+    #     request = requests.post(access_token_url, data=access_token_data, headers=access_token_headers)
+    #     # print(request.json())
+    #     valid_request = request.status_code in range(200, 299)
+    #     return request.status_code
+    #     # access_token_response = request.json()
+    #
+    #     if valid_request:
+    #         now = datetime.datetime.now()
+    #         access_token = access_token_response['access_token']
+    #         self.access_token = access_token
+    #         access_token_type = access_token_response['token_type']
+    #         access_token_expiry = access_token_response['expires_in']
+    #         token_expires = now + datetime.timedelta(seconds=access_token_expiry)
+    #         self.access_token_expiry = token_expires
+    #         self.token_is_expired = token_expires < now  # returns True if token has expired
+    #         return True
+    # raise Exception("Could not Authenticate, please check credentials")
+
+    # @staticmethod
+    # def access_token_headers():
+    #     return {
+    #         "Content-Type": "application/x-www-form-urlencoded"
+    #     }
+
+    def get_access_token_body(self):
+        client_id = self.client_id
+        auth_code = input("please copy the code")
+        redirect_uri = "http://localhost:8888/callback"
+        data = {
+            "client_id": client_id,
+            "grant_type": "authorization_code",
+            "code": auth_code,
+            "redirect_uri": redirect_uri,
+            "code_verifier": code_verifier
+        }
+        return data
+
+    def get_access_token(self):
         access_token_url = self.access_token_url
-        access_token_data = self.get_access_token_data()
-        access_token_headers = self.get_access_token_headers()
-        request = requests.post(access_token_url, data=access_token_data, headers=access_token_headers)
-        # print(request.json())
-        valid_request = request.status_code in range(200, 299)
-        return request.status_code
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        request = requests.post(access_token_url, data=self.get_access_token_body(), headers=headers)
         access_token_response = request.json()
+        valid_request = request.status_code in range(200,299)
 
         if valid_request:
             now = datetime.datetime.now()
@@ -92,35 +140,23 @@ class PkceAuthCode(object):
             self.access_token_expiry = token_expires
             self.token_is_expired = token_expires < now  # returns True if token has expired
             return True
-        # raise Exception("Could not Authenticate, please check credentials")
+        raise Exception("Could not Authenticate, please check credentials")
 
-    def get_access_token_body(self):
-        # client_id = self.client_id
-        grant_type = self.get_access_token_data()
-        auth_code = input("please copy the code")
-        redirect_uri = "http://localhost:8888/callback"
-        data = urlencode(
-            {
-                "client_id": client_id,
-                "grant_type": grant_type,
-                "code": auth_code,
-                "redirect_uri": redirect_uri,
-                "code_verifier": get_code_verifier()
-            }
-        )
-        return data
-
-    def get_access_token(self):
-        access_token_url = self.access_token_url
-        access_token_body = self.get_access_token_body()
-
-        request = requests.post(access_token_url, data = access_token_body)
-        print(request.status_code)
-
+    def refresh_access_token(self):
+        auth_success = self.get_auth()
+        if not auth_success:
+            raise Exception("Authentication failed")
+        token = self.access_token
+        expired = self.access_token_expiry
+        now = datetime.datetime.now()
+        if expired < now:
+            self.get_auth()
+            return self.refresh_access_token()
+        return token
 
 
 client = PkceAuthCode()
 # print(client.get_auth())
-# client.open_auth()
-print(client.get_access_token_body())
-# client.get_access_token()
+client.open_auth()
+# print(client.get_access_token_body())
+print(client.get_access_token())
--- src/credentials.py
+++ src/credentials.py
@@ -4,16 +4,6 @@ client_id = "48a43f17b4564edfad5c0ab4311df8f8"  # Unique identifier of the appli
 client_secret = "88e773d34a074a6aacb009ef29d625cf"  # key used to pass in secure calls to SpotifyApi. Can be
 # regenerated.
 
-# def get_auth_code():
-#     if __name__ == "__main__":
-#         auth_code = input("please copy the code")
+code_verifier = pkce.generate_code_verifier(length=128)
+code_challenge = pkce.get_code_challenge(code_verifier)
 
-
-def get_code_verifier():
-    code_verifier = pkce.generate_code_verifier(length=128)
-    return code_verifier
-
-def get_challenge_code():
-    code_verifier = get_code_verifier()
-    code_challenge = pkce.get_code_challenge(get_code_verifier())
-    return code_challenge
