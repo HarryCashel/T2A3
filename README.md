7c5df1b332fcd13641063a123b59815ead69702b
HarryCashel <65519528+HarryCashel@users.noreply.github.com>
2020-09-30 23:51:18 +1000

    README and user input for credentials

---
+++ README.md
@@ -0,0 +1,20 @@
+#Spotify CLI Application
+
+##Description
+
+This app uses SpotifyforDevelopers to allow a user to search for artists, songs and albums. 
+The user can create, view and edit playlists.
+
+To use this app you will need to link your Spotify account with a Spotify Developer account.
+
+Create a Spotify Developer account here https://developer.spotify.com/  
+You will be required to enter the client_id and client_secret into the terminal.
+
+##Installation
+#### Dependencies
+*base64  
+*datetime  
+*json  
+*urlencode  
+*requests  
+*pkce  
--- src/credentials.py
+++ src/credentials.py
@@ -1,9 +1,10 @@
 import pkce
 
-client_id = "48a43f17b4564edfad5c0ab4311df8f8"  # Unique identifier of the application
-client_secret = "88e773d34a074a6aacb009ef29d625cf"  # key used to pass in secure calls to SpotifyApi. Can be
+# client_id = "48a43f17b4564edfad5c0ab4311df8f8"  # Unique identifier of the application
+# client_secret = "88e773d34a074a6aacb009ef29d625cf"  # key used to pass in secure calls to SpotifyApi. Can be
 # regenerated.
-
+client_id = input("Please copy and paste your client id")
+client_secret = input("Please copy and paste your client secret")
 code_verifier = pkce.generate_code_verifier(length=128)
 code_challenge = pkce.get_code_challenge(code_verifier)
 
