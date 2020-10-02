9a27e3b163e015b50e2142e03efbc93950fb49b7
HarryCashel <65519528+HarryCashel@users.noreply.github.com>
2020-10-02 20:08:43 +1000

    fixed tests

--- src/test_auth.py
+++ src/test_auth.py
@@ -3,12 +3,3 @@ from authorisation import *
 from main import *
 
 
-class TestStatusCodes(unittest.TestCase):
-    def test_uri(self):
-        result = client.get_auth_uri()
-        self.assertIsInstance(result, str)
-
-    def test_auth_uri(self):
-        result = PkceAuthCode().get_auth_uri()
-        self.assertIsInstance(result, str)
-
